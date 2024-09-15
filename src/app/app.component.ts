9695ebf345221f3362cbbf8edf48a2c24f2471ff
BrettGubler <71382316+BrettGubler@users.noreply.github.com>
2024-09-15 14:44:04 -0600

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template:
+  `<div *ngIf="serverStatus; else elseBlock">
+"Yes"</div>
+<ng-template #elseBlock>"Okay Dokey"</ng-template>
+
+ "TEst"
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  serverStatus:boolean = false;
 }
